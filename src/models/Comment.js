4a3bde602cc003fbea0a5c8490a4a1ffa282077a
tianahR <farahrabenjam@yahoo.com>
2024-11-19 21:26:27 -0500

    Add Comment Schema

---
+++ src/models/Comment.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['Approved', 'Pending', 'Rejected'],
+    default: 'Pending',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
