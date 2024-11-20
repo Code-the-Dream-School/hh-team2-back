87540c540d64a99066fd7d8e6bd72dd3898e4bbe
tianahR <farahrabenjam@yahoo.com>
2024-11-19 21:58:16 -0500

    Add Group Schema, Change Comment Schema to have timestamp field

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,29 +1,28 @@
 const mongoose = require('mongoose');
 
-const CommentSchema = new mongoose.Schema({
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Post',
-    required: true,
+const CommentSchema = new mongoose.Schema(
+  {
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['Approved', 'Pending', 'Rejected'],
+      default: 'Pending',
+    },
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ['Approved', 'Pending', 'Rejected'],
-    default: 'Pending',
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Comment', CommentSchema);
